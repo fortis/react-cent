443376c19693035f78db3651ca77b2b55f621f23
Alan Bondarchuk <imacoda@gmail.com>
2017-10-31 00:10:50 +0500

    Update wrapper

--- src/CentComponent.js
+++ src/CentComponent.js
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types'
 
 const cent = (ComponentToWrap) => {
   return class CentComponent extends React.Component {
+    static contextTypes = {
+      cent: PropTypes.object.isRequired
+    }
+
     render = () => <ComponentToWrap {...this.props} cent={this.context.cent} />
   }
-}
-
-cent.contextTypes = {
-  cent: PropTypes.object.isRequired
 }
 
 export default cent
